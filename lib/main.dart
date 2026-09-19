0ec8c2ecd411242ea34aadf4e47baf27ca09c1c1
radenalberto <lumintzbelajar@gmail.com>
2026-09-19 12:30:58 +0700

    update sampe keranjang belanja

--- lib/main.dart
+++ lib/main.dart
@@ -1,150 +1,66 @@
 import 'package:flutter/material.dart';
 
+class Product {
+  final String name;
+  final String subtitle;
+  final int price;
+  final String imageUrl;
+  int likes;
+  bool isLiked;
+  bool isSelected;
+  int quantity;
+
+  // Masuk Ke Bagian Constructornya
+  Product({ //Fungsi dari ada { kurung kurawal di dalam ( kurung biasa adalah
+    // untuk membuat label agar tidak hanya menjadi
+    // angka dan teks acak seperti Product('Headphone', 350000)
+
+    // Memakai required karena informasi penting yang pasti berbeda di tiap produknya
+    required this.name,
+    required this.subtitle,
+    required this.price,
+    required this.imageUrl,
+    // tidak pakai required karena informasi yang tidak pasti berbeda di tiap produknya
+    this.likes = 0,
+    this.isLiked = false,
+    this.isSelected = false,
+    this.quantity = 1,
+  });
+}
+
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
+// Memakai StatelessWidget karena tugas MyApp hanya sebagai
+// pembungkus luar/yang mengatur tema
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp>{
-  int likes = 10;
-  bool selected = false;
-  String message = '';
 
   @override
   Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: CartPage(), //akan memanggil layar keranjang
+    );
+  }
+}
 
-    final screenWidth = MediaQuery.of(context).size.width;
+class CartPage extends StatefulWidget {
+  const CartPage({super.key});
 
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text(
-            'Profile',
-            style: TextStyle(
-              color: Colors.white,
-            ),
-          ),
-          backgroundColor: Colors.blue,
-        ),
-        body:SafeArea(child:
-        Container(
-          //  width: screenWidth *0.9,
-          margin: const EdgeInsets.all(20),
-          padding: const EdgeInsets.all(20),
-          color: Colors.blue.shade200,
-          child: Column(
-            children: [
+  @override
+  State<CartPage> createState() => _CartPageState();
+}
 
-              Row(
-                children: [
-                  const Text(
-                    'My Profile',
-                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-                  ),
-
-                  Spacer(),
-                  Text('Edit')
-                ],
-              ),
-              Stack(
-                children: [
-                  GestureDetector(
-                      onTap: () {
-                        setState(() {
-                          selected = !selected;
-                        });
-                      },
-                      child: Container(
-                        decoration: BoxDecoration(
-                            shape: BoxShape.circle,
-                            border: Border.all(
-                                color: selected? Colors.blue : Colors.transparent,
-                                width: 5
-                            )
-                        ),
-                        child: Image.network('https://i.pravatar.cc/',
-                          errorBuilder: (BuildContext context, Object error,
-                              StackTrace? stackTrace){
-                            return Container(
-                              width: 120,
-                              height: 120,
-                              decoration: BoxDecoration(
-                                color:  Colors.grey.shade500,
-                                shape: BoxShape.circle,
-                              ),
-                              child: const Icon(Icons.person,
-                                color:Colors.grey, size: 100,),
-                            );
-                          },
-                          width: 120,
-                          height: 120,
-                        ),
-                      )
-
-                  ),
-
-                  const Positioned(
-                      right: 0,
-                      bottom: 0,
-                      child: Icon(Icons.edit) ),
-                ],
-              ),
-
-              Container(
-                child: Column(
-                  children: [
-                    SizedBox(width: 10,
-                      child: Icon(Icons.favorite),
-                    ),
-                    SizedBox(width: 20,
-                      child: Text('$likes'),
-                    )
-                  ],
-                ),
-              ),
-
-              const SizedBox(height: 20),
-              const Text('Daniel Witarsa'),
-              const SizedBox(height: 20),
-              const Text('daniel.825240106@stu.untar.ac.id'),
-
-              const SizedBox(height: 20),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Expanded(
-                      flex: 1,
-                      child: Container(
-                        padding: const EdgeInsets.all(15),
-                        margin: const EdgeInsets.only(right: 5),
-                        color: Colors.blue,
-                        child: const Text('Workout',
-                          textAlign: TextAlign.center,
-                        ),
-                      )),
-                  Expanded(
-                      flex: 1,
-
-                      child: Container(
-                        padding: const EdgeInsets.all(15),
-                        color: Colors.green,
-                        child: const Text('Progress',
-                          textAlign: TextAlign.center,
-                        ),
-                      )),
-
-                ],
-              )
-            ],
-          ),
-        ),
-        ),
+class _CartPageState extends State<CartPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+      child: Text('Keranjang Belanja'),
       ),
     );
   }
 }
+
